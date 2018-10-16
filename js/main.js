f3bd44dcd020c739498db9042ee1e8d316d5a2c7
Adrian <adrcilia@gmail.com>
2018-10-16 16:42:02 +0100

    1

--- js/main.js
+++ js/main.js
@@ -5,6 +5,23 @@ var HEROES;
 var ROOT2 = false;
 var ANCIENT_SOULS;
 
+var xyl;
+var chor;
+var pony;
+var ACs;
+var xylBonus;
+var cps;
+var gildBonus;
+
+var goldBonus140 = Math.log10(1.6 / 1.15) * 139;
+    goldBonus140 -= 2; // 1% TCC
+var hsSplit = Math.log10(1 / 11);
+var hsActiveDmgAdjust = Math.log10(2) / 2 * 3 + Math.log10(2.5) * 2 / 5;
+var hsIdleDmgAdjust = Math.log10(2) / 2 * 2 + Math.log10(2.5) * 2 / 5;
+var hsGoldAdjust = Math.log10(2) / 2 * 3;
+
+var comboTime;
+
 function prepareHPScale() {
     if (!ROOT2) {
         HP_SCALE = [[1, 140], [1.55, 1.145]];
@@ -36,9 +53,60 @@ function getHeroAttr(hnum, attr) {
     return HEROES[hnum][HERO_TABLE_COLUMNS[attr]];
 }
 
+function getAdvancedInputs() {
+    let xyliqilLevel = parseFloat($("#xylInput").val() || 0);
+    if (!(xyliqilLevel >= 0)) { xyliqilLevel = 0; }
+    xyliqilLevel = Math.floor(xyliqilLevel);
+    $("#xylInput").val(xyliqilLevel.toString().replace(/\+/g,''));
+
+    let chorLevel = parseFloat($("#chorInput").val() || 0);
+    if (!(chorLevel >= 0)) { chorLevel = 0; }
+    if (chorLevel > 150) { chorLevel = 150; }
+    chorLevel = Math.floor(chorLevel);
+    $("#chorInput").val(chorLevel.toString().replace(/\+/g,''));
+
+    let ponyLevel = parseFloat($("#ponyInput").val() || 0);
+    if (!(ponyLevel >= 0)) { ponyLevel = 30; }
+    ponyLevel = Math.floor(ponyLevel);
+    $("#ponyInput").val(ponyLevel.toString().replace(/\+/g,''));
+    
+    let autoClickers = parseFloat($("#ACInput").val() || 0);
+    if (!(autoClickers >= 0)) { autoClickers = 0; }
+    autoClickers = Math.floor(autoClickers);
+    $("#ACInput").val(autoClickers.toString().replace(/\+/g,''));
+    
+    return [xyliqilLevel, chorLevel, ponyLevel, autoClickers];
+}
+
 function calculateProgression() {
+    
     ANCIENT_SOULS = parseFloat($("#inputAS").val());
-    var lghs = parseFloat($("#inputLgHS").val());
+    
+    let heroSoulsInput = $("#inputLgHS").val();
+    var lghs = "";
+    if (heroSoulsInput.indexOf("e") > -1) {
+        let mantissa = heroSoulsInput.substr(0, heroSoulsInput.indexOf("e"));
+        let exponent = heroSoulsInput.substr(heroSoulsInput.lastIndexOf("e") + 1);
+        mantissa = parseFloat(mantissa || 0);
+        exponent = parseFloat(exponent || 0);
+        if ((isNaN(mantissa)) || isNaN(exponent)) {
+            $("#inputLgHS").val("");
+        }
+        lghs = exponent + Math.log10(mantissa);
+        mantissa = Math.pow(10, lghs % 1).toFixed(3);
+        exponent = Math.floor(lghs);
+        $("#inputLgHS").val(mantissa + "e" + exponent);
+    } else {
+        lghs = parseFloat(heroSoulsInput || 0);
+        $("#inputLgHS").val(lghs.toFixed(4));
+    }
+    
+    var advancedInputs = getAdvancedInputs();
+    xyl = advancedInputs[0];
+    chor = advancedInputs[1];
+    pony = advancedInputs[2];
+    ACs = advancedInputs[3];
+    cps = ACs > 4 ? Math.log10(1.5) * (ACs - 1) + 1: Math.log10(ACs + 1) + 1;
     
     var errMsg = "";
     if (isNaN(ANCIENT_SOULS) || isNaN(lghs) ||
@@ -59,11 +127,16 @@ function calculateProgression() {
     ROOT2 = $("#gameRoot2").prop("checked");
     HEROES = ROOT2 ? HEROES_ROOT2 : HEROES_VANILLA;
     
+    xylBonus = xyl * Math.log10(1.5);
+    
     prepareHPScale();
 
     var tp = ROOT2 ? root2_tp(ANCIENT_SOULS) : 
         0.25 - 0.23 * Math.exp(-0.0003 * ANCIENT_SOULS);
     $("#outputTP").val(tp.toFixed(6));
+    
+    var gilds = Math.max(1,Math.floor(lghs / Math.log10(1 + tp) / 10));
+    gildBonus = Math.log10(gilds) + (ROOT2 ? Math.log10(1.01) * gilds : 0);
     
     var data = [];
     var start = 0;
@@ -73,19 +146,49 @@ function calculateProgression() {
     
     var t0 = performance.now();
     
+    var effectivelghs;
+    
     for (i = 0; i < 250; i++) {
-        hnum = heroReached(lghsStart, start);
-        zone = zoneReached(lghsStart, hnum);
-        hlevel = (zone * Math.log10(GOLD_SCALE) + 1.5 * lghsStart + 21 
-            - getHeroAttr(hnum, "lv1cost")) / 
+        comboTime = 0;
+        effectivelghs = lghsStart + Math.log10(1 / 0.95) * chor + hsSplit;
+        
+        if (ROOT2) {
+            xylBonus = 0.2505 * (1 - Math.exp(xyl * -0.04)) * (lghsStart + hsSplit + Math.log10(2.5) * 2 / 5);
+        }
+        
+        hnum = heroReached(effectivelghs, start);
+        zone = zoneReached(effectivelghs, hnum);
+        
+        hnumTL = heroReached(effectivelghs, startTL, active=false);
+        zoneTL = zoneReached(effectivelghs, hnumTL, active=false);
+        
+        if (zoneTL > zone) {
+            if (zoneTL > MAX_ZONE) zoneTL = MAX_ZONE
+            hnum = hnumTL;
+            zone = zoneTL;
+            start = startTL
+            if (i === 0 ) console.log("go idle");
+        } else {
+            if (zone > MAX_ZONE) zone = MAX_ZONE
+            let time = (zone - zoneTL) / 8000 * 3600;
+            comboTime = Math.log10(time);
+            zone = zoneReached(effectivelghs, hnum);
+        }
+        
+        var goldBonus = zone > zoneTL && !ROOT2 // Autoclickers or Xyliqil gold increase
+            ? Math.min(306, cps)
+            : xylBonus;
+        
+        hlevel = (zone * Math.log10(GOLD_SCALE) + 1.5 * effectivelghs + hsGoldAdjust + goldBonus140 
+            - getHeroAttr(hnum, "lv1cost") + goldBonus - Math.log10(15)) / 
             Math.log10(getHeroAttr(hnum, "costScale"));
         lghsEnd = (zone / 5 - 20) * Math.log10(1 + tp) 
-            + Math.log10(20 * 10000 * (1 + tp) / tp);
+            + Math.log10(20 * (1 + tp) / tp);
+        lghsEnd += pony > 100
+            ? Math.log10(pony) * 2 + 1
+            : Math.log10(pony * pony * 10 + 1);
         lghsChange = lghsEnd - lghsStart > 50 ? lghsEnd - lghsStart 
             : Math.log10(1 + Math.pow(10, lghsEnd - lghsStart));
-        
-        hnumTL = heroReached(lghsStart, startTL, active=false);
-        zoneTL = zoneReached(lghsStart, hnumTL, active=false);
         
         data.push([
             i,
@@ -94,9 +197,9 @@ function calculateProgression() {
             zone.toFixed(0),
             hlevel.toFixed(0),
             lghsChange.toFixed(2),
-            ROOT2 ? "" : zoneTL.toFixed(0)
+            zoneTL.toFixed(0)
         ])
-        if (zone == MAX_ZONE) { // Stop if encountering infinite ascension
+        if (zone >= MAX_ZONE) { // Stop if encountering infinite ascension
             break;
         }
         lghsStart += lghsChange;
@@ -116,7 +219,10 @@ function heroReached(lgHS, start=0, active=true) {
     var i = start;
     for (; i < HEROES.length; i++) {
         zone = zoneReached(lgHS, i, active);
-        gold = zone * Math.log10(GOLD_SCALE) + 1.5 * lgHS + 21;
+        gold = zone * Math.log10(GOLD_SCALE) + 1.5 * lgHS + hsGoldAdjust + goldBonus140 - Math.log10(15);
+        gold += active // Autoclickers or Xyliqil gold increase
+            ? Math.min(306, cps)
+            : ROOT2 ? 0 : xylBonus;
         if (zone == MAX_ZONE || i == HEROES.length - 1 || 
             gold < heroUpgradeBaseCost(i + 1)) {
             break;
@@ -133,11 +239,21 @@ function zoneReached(lgHS, i, active=true) {
     let efficiency = getHeroAttr(i, 'dps') - 
         R * (getHeroAttr(i, "lv1cost") + 175 * Math.log10(getHeroAttr(i, "costScale")));
 
-    let startingGold = 21.12 + 1.5 * lgHS;
+    let startingGold = hsGoldAdjust + 1.5 * lgHS + goldBonus140 - Math.log10(15);
+    startingGold += active // Autoclickers or Xyliqil gold increase
+        ? Math.min(306, cps)
+        : ROOT2 ? 0 : xylBonus;
     startingGold += ROOT2 * (i >= 47) * 98; // BomberMax global gold boost on Root2
     
     let RHS = efficiency + (2.4 + active * 0.5) * lgHS 
-         + 1.86 - 2 + startingGold * R;   // Minus 2 to account for boss HP    
+        - 2 + startingGold * R;   // Minus 2 to account for boss HP
+    RHS += active
+        ? hsActiveDmgAdjust
+        : hsIdleDmgAdjust;
+    RHS += active // Autoclickers or Xyliqil damage increase
+        ? Math.min(306, cps) * 2
+        : xylBonus * (ROOT2 ? 1 : 2) + (ACs > 2e9 ? 0 : cps);
+    RHS += gildBonus + comboTime;
     RHS += ROOT2 * (i >= 48) * 43.64;   // Gog global DPS boost on Root2
     
     let reqzone = (heroUpgradeBaseCost(i) - startingGold) / 
@@ -188,3 +304,9 @@ function dataArrayToHTML(data) {
     datastr = "<tr>" + data2.join("</tr><tr>") + "</tr";
     return datastr;
 }
+
+function enterKey(ev) {
+    if (ev.which === 13) calculateProgression();
+}
+
+$(document).keyup(enterKey);
---
+++ js/readSave.js
@@ -0,0 +1,29 @@
+function readSave() {
+	let txt = $("#saveInput").val();
+	let data;
+	if (txt.indexOf("Fe12NAfA3R6z4k0z") > -1 || txt.substring(0, 32) == "7a990d405d2c6fb93aa8fbb0ec1a3b23") {
+		if (txt.substring(0, 32) == "7a990d405d2c6fb93aa8fbb0ec1a3b23") {
+			try {
+				data = JSON.parse(pako.inflate(atob(txt.substring(32)), {to: 'string'}));
+			} catch(e) {
+				$("#saveInput").val("");
+				return;
+			}
+		} else {
+			var result = txt.split("Fe12NAfA3R6z4k0z");
+			txt = "";
+			for (var i = 0; i < result[0].length; i += 2) {
+				txt += result[0][i];
+			}
+			data = JSON.parse(atob(txt));
+		}
+		$("#inputAS").val(data.ancientSoulsTotal);
+        $("#inputLgHS").val(data.stats.currentAscension.heroSoulsStart);
+        $("#xylInput").val(data.outsiders.outsiders[1].level);
+        $("#chorInput").val(data.outsiders.outsiders[2].level);
+        $("#ponyInput").val(data.outsiders.outsiders[5].level);
+        $("#ACInput").val(data.autoclickers);
+		calculateProgression();
+	} else if (txt)
+		$("#saveInput").val("");
+}
